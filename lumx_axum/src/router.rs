cdaa2538bd7a5d950f51c8c3ad861041354093fa
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-02-14 02:27:36 +0100

    fix: merge multiple router

--- lumx_axum/src/router.rs
+++ lumx_axum/src/router.rs
@@ -1,11 +1,20 @@
+use axum::Router;
 use std::sync::{Arc, RwLock};
 
 #[derive(Clone)]
-pub struct RouterRef(pub Arc<RwLock<axum::Router>>);
+pub struct RouterRef(pub Arc<RwLock<Router>>);
 
 impl RouterRef {
-    pub fn new(router: axum::Router) -> Self {
+    pub fn new(router: Router) -> Self {
         Self(Arc::new(RwLock::new(router)))
+    }
+
+    pub fn add_router(&self, other_router: Router) {
+        let router_registered = self.0.read().unwrap().to_owned();
+
+        let mut writer = self.0.write().unwrap();
+
+        *writer = router_registered.merge(other_router);
     }
 }
 
