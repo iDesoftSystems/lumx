beb7664f28ad8707b15b83cce9eebf2ffd8e981a
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-02-23 21:55:20 +0100

    fix: merge multiple router (#15)

--- lumx_axum/src/plugin.rs
+++ lumx_axum/src/plugin.rs
@@ -1,42 +1,42 @@
-use std::{net::SocketAddr, sync::Arc};
-
+use crate::{router::RouterRef, state::AppState};
 use axum::{async_trait, Extension, Router};
 use lumx_core::{
     plugable::plugin::Plugin,
     program::{Program, ProgramBuilder},
     types::ProgramFailure,
 };
-
-use crate::{router::RouterRef, state::AppState};
+use std::ops::Deref;
+use std::{net::SocketAddr, sync::Arc};
+use tower_http::trace::TraceLayer;
+use tracing::debug;
 
 pub struct WebPlugin;
 
 #[async_trait]
 impl Plugin for WebPlugin {
     async fn build(&self, app: &mut ProgramBuilder) {
-        let router = app.get_component::<RouterRef>();
+        let router_ref = app.get_expect_component::<RouterRef>();
 
-        let router = match router {
-            Some(rs) => {
-                let r_val = rs.0.read().unwrap();
-                r_val.to_owned()
-            }
-            None => Router::new(),
-        };
+        let mutex_router_ref = router_ref.0.to_owned();
+        let mutex_guard = mutex_router_ref.lock().unwrap();
+        let router = mutex_guard.deref().to_owned();
 
         app.add_schedule(move |app_c: Arc<Program>| Box::new(Self::schedule(app_c, router)));
     }
 }
 
 impl WebPlugin {
-    async fn schedule(app: Arc<Program>, router: axum::Router) -> Result<String, ProgramFailure> {
+    async fn schedule(app: Arc<Program>, router: Router) -> Result<String, ProgramFailure> {
         let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
         let listener = tokio::net::TcpListener::bind(addr)
             .await
             .expect(format!("bind tcp listener failed: {}", addr).as_str());
 
-        let router = router.layer(Extension(AppState { app }));
+        debug!(?router, "registered routes");
+        let router = router
+            .layer(Extension(AppState { app }))
+            .layer(TraceLayer::new_for_http());
 
         println!("Listening on {}", listener.local_addr().unwrap());
         println!("Ctrl-C to shutdown server");
